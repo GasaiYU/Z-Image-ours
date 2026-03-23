01e376bf964289c450db0cd6f347f9342ef308fe
高鸣驹 <gaomingju@gaomingjudeMacBook-Pro.local>
2026-03-23 16:53:49 +0800

    add simpplied

--- scripts/test_geneval.sh
+++ scripts/test_geneval.sh
@@ -1,3 +1,3 @@
 python benchmarks/geneval/generation/zimage_generate.py \
-    benchmarks/geneval/prompts/evaluation_metadata_long.jsonl \
-    --outdir outputs/outputs_geneval_long --tags counting colors gender_race
+    benchmarks/geneval/prompts/evaluation_metadata_short.jsonl \
+    --outdir outputs/outputs_geneval_short --tags counting colors gender_race
