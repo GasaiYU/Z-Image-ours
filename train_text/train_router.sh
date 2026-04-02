819ebd58580e157e9e667b7b5abafbe0f7537b89
高鸣驹 <gaomingju@gaomingjudeMacBook-Pro.local>
2026-04-02 23:13:00 +0800

    add supon

--- train_text/train_router.sh
+++ train_text/train_router.sh
@@ -1,7 +1,7 @@
 python train_text/train_router.py \
     --triplet_dir data/train_triplets \
     --output_dir checkpoints/router \
-    --loss_type infonce \
+    --loss_type supcon \
     --temperature 0.07 \
     --batch_size 64 \
     --epochs 20 \
