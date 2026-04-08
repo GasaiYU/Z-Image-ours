0062c3c98b3e705f6396ff91dfebdd8950624431
高鸣驹 <gaomingju@gaomingjudeMacBook-Pro.local>
2026-04-08 16:52:56 +0800

    add

--- data/gen_image.sh
+++ data/gen_image.sh
@@ -3,4 +3,4 @@ export MKL_NUM_THREADS=4
 export HF_HOME=/mmu-vcg/gaomingju/data/T2I/hf_cache/
 
 
-python data/generate_triplet_images.py --n_gpus 8 --batch_size 
+python data/generate_triplet_images.py --n_gpus 8 --batch_size 1
